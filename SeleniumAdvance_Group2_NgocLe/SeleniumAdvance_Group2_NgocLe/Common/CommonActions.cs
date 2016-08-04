84766853cfa67c65eeaafeb6257ac6d5273b556f
minhthinguyen <nguyenthiminhthi2009@gmail.com>
2016-08-04 15:39:40 +0700

    Add method FindElement to CommonActions

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/Common/CommonActions.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/Common/CommonActions.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 
 namespace SeleniumAdvance_Group2.Common
 {
     public class CommonActions
     {
+        public void FindElement(By control)
+        {
+
+        }
     }
 }
