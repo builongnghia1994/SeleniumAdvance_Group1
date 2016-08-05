dba8408e4a42745519ad82ebd356dc0f9ea0d378
minhtrangle <trangleqn@hotmail.com>
2016-08-05 10:32:31 +0700

    add user/pass for account Trang

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/Common/Constant.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/Common/Constant.cs
@@ -5,10 +5,11 @@ namespace SeleniumAdvance_Group2.Common
     public class Constant
     {
         public static IWebDriver WebDriver;
-        public static string Url = "http://192.168.190.205:54000/TADashboard/2f9njff6y9.page";
+        public static string DashboardURL = "http://192.168.190.205:54000/TADashboard/2f9njff6y9.page";
        
         public static string Respository = "SampleRepository";
 
-
+        public static string userTrang = "trang.le";
+        public static string passTrang = "1";
     }
 }
