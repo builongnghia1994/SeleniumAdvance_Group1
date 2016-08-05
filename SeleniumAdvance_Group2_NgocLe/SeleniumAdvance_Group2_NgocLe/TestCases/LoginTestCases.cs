1f53868b64d146bbc3231b136e1d4ece1ed318ff
minhthinguyen <nguyenthiminhthi2009@gmail.com>
2016-08-05 13:36:49 +0700

    updated

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/LoginTestCases.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/LoginTestCases.cs
@@ -7,14 +7,17 @@ namespace SeleniumAdvance_Group2.TestCases
     [TestClass]
     public class LoginTestCases : TestBases
     {
+       private string username = "thi.nguyen";
+        private string pass = "1";
+
         [TestMethod]
         public void DA_LOGIN_TC001_Verify_that_user_can_login_specific_repository_successfully_with_correct_credentials()
         {
 
             loginPage = OpenURL(Constant.DashboardURL);
-            generalPage = loginPage.Login("thi.nguyen", "1");
+            generalPage = loginPage.Login(username, pass);
             //VP
-            generalPage.VerifyWelComeUser("thi.nguyen");
+            generalPage.VerifyWelComeUser(username);
             loginPage = generalPage.LogOut();
 
         }
