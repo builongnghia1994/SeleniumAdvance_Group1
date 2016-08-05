da10bf13db13fe73d396d394425a02fba393ad52
minhthinguyen <nguyenthiminhthi2009@gmail.com>
2016-08-05 17:24:01 +0700

    up

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/Common/Constant.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/Common/Constant.cs
@@ -9,9 +9,11 @@ namespace SeleniumAdvance_Group2.Common
         public static string DashboardURL = "http://192.168.190.205:54000/TADashboard/2f9njff6y9.page";
        
         public static string Respository = "SampleRepository";
-
         public static string userTrang = "trang.le";
         public static string passTrang = "1";
         public static string MsgRequiredFieldPanel = "Display Name is a required field.";
+        public static string parentname_newpage = "Select parent";
+        public static string pageafter_newpage = "Overview";
+        public static string numbercolumn_newpage = "2";
     }
 }
