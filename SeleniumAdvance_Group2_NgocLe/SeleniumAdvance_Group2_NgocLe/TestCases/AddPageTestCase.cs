e9f65654f5ff8d7bdf41b0b3c1a92d074d2d639b
minhthinguyen <nguyenthiminhthi2009@gmail.com>
2016-08-05 17:35:05 +0700

    up

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/AddPageTestCase.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/AddPageTestCase.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace SeleniumAdvance_Group2.TestCases
 {
     [TestClass]
-    public class UnitTest1
+    public class AddPageTestCase
     {
         [TestMethod]
         public void TestMethod1()
