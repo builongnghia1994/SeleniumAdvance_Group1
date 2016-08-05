2aa43069499fc18f43501c683f3a5332e6330647
minhthinguyen <nguyenthiminhthi2009@gmail.com>
2016-08-05 17:17:36 +0700

    up

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/Common/Constant.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/Common/Constant.cs
@@ -13,8 +13,6 @@ namespace SeleniumAdvance_Group2.Common
         public static string userTrang = "trang.le";
         public static string passTrang = "1";
 
-        public static string parentname_newpage = "Select parent";
-        public static string pageafter_newpage = "Overview";
-        public static string numbercolumn_newpage = "2";
+        
         }
 }
