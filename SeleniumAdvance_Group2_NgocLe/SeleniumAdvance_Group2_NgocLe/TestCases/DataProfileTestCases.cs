aeea765e5cbc3885c0149ad695a21cbbbb9f300e
builongnghia1994 <builongnghia1994@gmail.com>
2016-08-05 13:00:57 +0700

    UPdate TC05

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/DataProfileTestCases.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/DataProfileTestCases.cs
@@ -10,9 +10,9 @@ namespace SeleniumAdvance_Group2.TestCases
         [TestMethod]
         public void DA_DP_TC065_Verify_that_all_Preset_Data_Profiles_are_populated_correctly()
         {
-            OpenURL(Constant.DashboardURL);
+            loginPage = OpenURL(Constant.DashboardURL);
 
-            loginpage.Login("nghia.bui", "1");
+            generalPage  = loginPage.Login("nghia.bui", "1");
 
             dataProfilePage.GotoDataProfilePage();
         }
