f2e26f837704dcbeb12beeb3b8d5b18012604506
minhtrangle <trangleqn@hotmail.com>
2016-08-05 15:36:06 +0700

    modify test case

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/PanelTestCases.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/PanelTestCases.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SeleniumAdvance_Group2.Common;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumAdvance_Group2.TestCases
 {
@@ -17,9 +19,14 @@ namespace SeleniumAdvance_Group2.TestCases
 
             loginPage = OpenURL(Constant.DashboardURL);
             generalPage = loginPage.Login(Constant.userTrang, Constant.passTrang);
-            panelPage = generalPage.GotoPanelPage();
-            ClickControl(panelPage.btnOK);
-
+              panelPage = generalPage.GotoPanelPage();
+             ClickControl(panelPage.BtnOK);
+            IAlert alert = Constant.WebDriver.SwitchTo().Alert();
+            Assert.AreEqual("Display Name is a required field.", alert.Text);
+            alert.Accept();
+           Constant.WebDriver.SwitchTo().DefaultContent();           
+            ClickControl(panelPage.BtnCancel);
+            //  generalPage.GotoPage("Trang/Trang1/Trang2");
         }
     }
 }
