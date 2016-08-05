0accd6784901c4f70332277260948968d068715c
builongnghia1994 <builongnghia1994@gmail.com>
2016-08-05 09:32:53 +0700

    Add the DataProfileTestCase

---
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/DataProfileTestCases.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumAdvance_Group2.TestCases
+{
+    [TestClass]
+    public class DataProfileTestCases: TestBases
+    {
+        [TestMethod]
+        public void DA_DP_TC065_Verify_that_all_Preset_Data_Profiles_are_populated_correctly()
+        {
+            loginpage.Login();
+
+
+        }
+    }
+}
