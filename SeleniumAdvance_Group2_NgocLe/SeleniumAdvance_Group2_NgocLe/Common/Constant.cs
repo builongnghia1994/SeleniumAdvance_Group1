50f2993f44df9727bb5dc761c501e79f3da562c9
builongnghia1994 <builongnghia1994@gmail.com>
2016-08-08 08:45:34 +0700

    Add preSetDataProfile variable

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/Common/Constant.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/Common/Constant.cs
@@ -15,5 +15,14 @@ namespace SeleniumAdvance_Group2.Common
         public static string parentname_newpage = "Select parent";
         public static string pageafter_newpage = "Overview";
         public static string numbercolumn_newpage = "2";
+
+        public static string[] preSetDataProfile = { "Action Implementation By Status", "Test Case Execution",
+            "Test Case Execution Failed Trend", "Test Case Execution History",
+            "Test Case Execution Results", "Test Case Execution Trend",
+            "Test Module Execution", "Test Module Execution Failure Trend",
+            "Test Module Execution History", "Test Module Execution Results",
+            "Test Module Execution Results Report", "Test Module Execution Trend",
+            "Test Module Implementation By Priority", "Test Module Implementation By Status",
+            "Test Module Status per Assigned Users", "Test Objective Execution" };
     }
 }
