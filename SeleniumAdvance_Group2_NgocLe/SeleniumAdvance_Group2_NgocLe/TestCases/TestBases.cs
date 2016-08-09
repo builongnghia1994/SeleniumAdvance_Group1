27305fd9dbcd08121c9db611d8939e0a2685b288
minhtrangle <trangleqn@hotmail.com>
2016-08-09 15:36:36 +0700

    move OpenBrowse to common actions

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/TestBases.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/TestBases.cs
@@ -39,46 +39,7 @@ namespace SeleniumAdvance_Group2.TestCases
         }
         #endregion
 
-        #region Methods
 
-        public void OpenBrowser(string browser)
-
-        {
-            switch (browser.ToLower())
-            {
-
-                case "chrome":
-                    ChromeOptions options = new ChromeOptions();
-                    options.AddArguments("--disable-extensions");
-                    Constant.WebDriver = new ChromeDriver(options);
-                    Constant.WebDriver.Manage().Window.Maximize();
-                    break;
-                case "ie":
-                    Constant.WebDriver = new InternetExplorerDriver();
-                    Constant.WebDriver.Manage().Window.Maximize();
-                    break;
-                case "firefox":
-                    Constant.WebDriver = new FirefoxDriver();
-                    Constant.WebDriver.Manage().Window.Maximize();
-                    break;
-                case "edgewin":
-                    Constant.WebDriver = new EdgeDriver();
-                    Constant.WebDriver.Manage().Window.Maximize();
-                    break;
-                default:
-                    Console.WriteLine(String.Format("Browser '{0}' not recognized. Spawning default Firefox browser.", browser));
-                    Constant.WebDriver = new FirefoxDriver();
-                    Constant.WebDriver.Manage().Window.Maximize();
-                    break;
-            }
-        }
-
-        public void CloseBrowser()
-        {
-            Constant.WebDriver.Manage().Cookies.DeleteAllCookies();
-            Constant.WebDriver.Quit();
-            #endregion
-        }
     }
 }
 
