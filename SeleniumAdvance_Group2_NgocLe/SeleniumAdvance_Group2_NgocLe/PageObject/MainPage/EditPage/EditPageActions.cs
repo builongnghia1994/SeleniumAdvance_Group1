8aeba7dba3be1f186ea8519c3e225dd1e48aee0c
minhthinguyen <nguyenthiminhthi2009@gmail.com>
2016-08-11 09:33:41 +0700

    up

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/PageObject/MainPage/EditPage/EditPageActions.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/PageObject/MainPage/EditPage/EditPageActions.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SeleniumAdvance_Group2.PageObject.GeneralPage;
+using SeleniumAdvance_Group2.PageObject.MainPage.NewPage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,30 @@ using System.Threading.Tasks;
 
 namespace SeleniumAdvance_Group2.PageObject.MainPage.EditPage
 {
-    class EditPageActions
+  public  class EditPageActions:GeneralPageActions
     {
+        public GeneralPageActions EditPage(string status, string pagename, string parentname, string afterpage, string numbercolum)
+
+        {
+            switch (status.ToLower())
+            {
+                case "public":
+                    ClickControl(NewPageUI.rdPublic);
+                    break;
+                default:
+                    break;
+            }
+            if (pagename != null)
+            { TypeValue(NewPageUI.txtPageName, pagename); }
+            if (parentname != null)
+            { SelectItemByDropdownList(NewPageUI.ddlParentName, parentname); }
+            if (numbercolum != null)
+            { SelectItemByDropdownList(NewPageUI.ddlnumbercolum, numbercolum); }
+            if (afterpage != null)
+            { SelectItemByDropdownList(NewPageUI.ddlDisplayAfter, afterpage); }
+            ClickControl(NewPageUI.btnOk);
+
+            return new GeneralPageActions();
+        }
     }
 }
