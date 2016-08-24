71e2423a50c4fed40e2a3c1e5701ce1d18d214f2
builongnghia1994 <builongnghia1994@gmail.com>
2016-08-24 17:56:17 +0700

    update grid

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/Draft/TestGrid.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/Draft/TestGrid.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium.Remote;
 using SeleniumAdvance_Group2.PageObject.Login;
 using SeleniumAdvance_Group2.PageObject.General;
 using SeleniumAdvance_Group2.Common;
+using System.Threading;
 
 namespace SeleniumAdvance_Group2.TestCases
 {
@@ -19,6 +20,7 @@ namespace SeleniumAdvance_Group2.TestCases
             IWebDriver webDriver;
             webDriver = new RemoteWebDriver(new Uri("http://192.168.190.114:4444/wd/hub"), DesiredCapabilities.Firefox());
             webDriver.Navigate().GoToUrl("http://192.168.190.205:54000/TADashboard/login.jsp");
+            Thread.Sleep(500);
             generalPage = loginPage.LoginDashBoard(Constant.Respos_SampleRepository, Constant.Username_nghia, Constant.Password);
         }
     }
--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/TestBases.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/TestBases.cs
@@ -14,36 +14,30 @@ namespace SeleniumAdvance_Group2.TestCases
 
         public TestContext TestContext { get; set; }
 
-        //[AssemblyInitialize]
+        [AssemblyInitialize]
         public static void AssemblyInitializeMeThod(TestContext testContext)
         {
+            //DesiredCapabilities capabilities = DesiredCapabilities.Chrome();
+            //capabilities.SetCapability(CapabilityType.Version, "51");
+            //capabilities.SetCapability(CapabilityType.Platform, new Platform(PlatformType.Windows));
+            Constant.WebDriver = new RemoteWebDriver(new Uri("http://192.168.190.114:4444/wd/hub"), DesiredCapabilities.InternetExplorer());
+            Constant.WebDriver.Manage().Window.Maximize();
             //OpenBrowser(Constant.Browser);
-            Constant.WebDriver = new RemoteWebDriver(new Uri("http://192.168.190.205:4444/wd/hub"), DesiredCapabilities.Firefox());
-
         }
 
-        //[AssemblyCleanup]
+        [AssemblyCleanup]
         public static void AssemblyCleanupMethod()
         {
             Constant.WebDriver.Quit();
         }
 
-        //[TestInitialize]
+        [TestInitialize]
         public void TestInitializeMethods()
         {
             //try
             //{
-            //    //start remote firefox browser
-            //    //DesiredCapabilities capabilities = DesiredCapabilities.Firefox();
-            //    //capabilities.SetCapability(CapabilityType.BrowserName, "Firefox");
-            //    //capabilities.SetCapability(CapabilityType.Version, "47.0.1");
-            //    //capabilities.SetCapability(CapabilityType.Platform, new Platform(PlatformType.Windows));
-            //    //Constant.WebDriver = new RemoteWebDriver(new Uri("http://192.168.190.114/wd/node"), capabilities, TimeSpan.FromSeconds(1000));
-
-            //    //Constant.WebDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(1000));
             //    //Constant.WebDriver.Manage().Window.Maximize();
             //    //string url = Constant.WebDriver.Url;
-
             //}
             //catch (Exception)
             //{
