92c2a1fe987fb8591ceb5ce5284ba2a60e40d586
ngocleedng <ngocleedng@gmail.com>
2016-08-24 10:07:33 +0700

    a

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/TestBases.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/TestBases.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
 using SeleniumAdvance_Group2.Common;
 using SeleniumAdvance_Group2.PageObject.Login;
@@ -12,54 +13,65 @@ namespace SeleniumAdvance_Group2.TestCases
     {
         public LoginPage loginPage;
 
-        //public TestContext TestContext { get; set; }
+        public TestContext TestContext { get; set; }
 
-        //[AssemblyInitialize]
-        //public static void AssemblyInitializeMeThod(TestContext testContext)
-        //{
-        //    OpenBrowser(Constant.Browser);
-        //}
+        [AssemblyInitialize]
+        public static void AssemblyInitializeMeThod(TestContext testContext)
+        {
+            Uri uri = new Uri("http://192.168.190.205:4444/wd/hub");
 
-        //[AssemblyCleanup]
-        //public static void AssemblyCleanupMethod()
-        //{
-        //    Constant.WebDriver.Quit();
-        //}
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities = DesiredCapabilities.Firefox();
+            capabilities.SetCapability(CapabilityType.BrowserName, "Firefox");
+            capabilities.SetCapability(CapabilityType.Platform, new Platform(PlatformType.Windows));
+            capabilities.SetCapability(CapabilityType.Version, "47.0.1");
+            RemoteWebDriver webDriver = new RemoteWebDriver(uri, capabilities);
+            //Constant.WebDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(1000));
+            webDriver.Manage().Window.Maximize();
 
-        //[TestInitialize]
-        //public void TestInitializeMethods()
-        //{
-        //    try
-        //    {
-        //        //start remote firefox browser
-        //        //DesiredCapabilities capabilities = DesiredCapabilities.Firefox();
-        //        //capabilities.SetCapability(CapabilityType.BrowserName, "Firefox");
-        //        //capabilities.SetCapability(CapabilityType.Version, "47.0.1");
-        //        //capabilities.SetCapability(CapabilityType.Platform, new Platform(PlatformType.Windows));
-        //        //Constant.WebDriver = new RemoteWebDriver(new Uri("http://192.168.190.114/wd/node"), capabilities, TimeSpan.FromSeconds(1000));
+            //OpenBrowser(Constant.Browser);
+        }
 
-        //        //Constant.WebDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(1000));
-        //        //Constant.WebDriver.Manage().Window.Maximize();
-        //        string url = Constant.WebDriver.Url;
+        [AssemblyCleanup]
+        public static void AssemblyCleanupMethod()
+        {
+            Constant.WebDriver.Quit();
+        }
 
-        //    }
-        //    catch (Exception)
-        //    {
-        //        OpenBrowser(Constant.Browser);
-        //    }
+        [TestInitialize]
+        public void TestInitializeMethods()
+        {
+            try
+            {
+                //start remote firefox browser
+                //DesiredCapabilities capabilities = DesiredCapabilities.Firefox();
+                //capabilities.SetCapability(CapabilityType.BrowserName, "Firefox");
+                //capabilities.SetCapability(CapabilityType.Version, "47.0.1");
+                //capabilities.SetCapability(CapabilityType.Platform, new Platform(PlatformType.Windows));
+                //Constant.WebDriver = new RemoteWebDriver(new Uri("http://192.168.190.114/wd/node"), capabilities, TimeSpan.FromSeconds(1000));
 
-        //    loginPage = OpenURL(Constant.DashboardURL);
-        //}
+                //Constant.WebDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(1000));
+                //Constant.WebDriver.Manage().Window.Maximize();
+                string url = Constant.WebDriver.Url;
 
-        //[TestCleanup]
-        //public void TestCleanupMethods()
-        //{
-        //    Constant.WebDriver.Manage().Cookies.DeleteAllCookies();
-        //    if (TestContext.CurrentTestOutcome.ToString() == "Failed")
-        //    {
-        //        Constant.WebDriver.Quit();
-        //    }
-        //}
+            }
+            catch (Exception)
+            {
+                //OpenBrowser(Constant.Browser);
+            }
+
+            loginPage = OpenURL(Constant.DashboardURL);
+        }
+
+        [TestCleanup]
+        public void TestCleanupMethods()
+        {
+            Constant.WebDriver.Manage().Cookies.DeleteAllCookies();
+            if (TestContext.CurrentTestOutcome.ToString() == "Failed")
+            {
+                Constant.WebDriver.Quit();
+            }
+        }
 
     }
 }
