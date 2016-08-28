364beeadb82e78a24e4e9384370abeaa7b38ccd3
minhthinguyen <nguyenthiminhthi2009@gmail.com>
2016-08-28 10:15:28 +0700

    replace TC

--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/Draft/DemoThi.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/Draft/DemoThi.cs
@@ -11,8 +11,37 @@ namespace SeleniumAdvance_Group2.TestCases
     {
         GeneralPage generalPage;
         NewPage newPage;
-       
-            [TestMethod]
+
+        [TestMethod]
+        public void DA_LOGIN_TC001_Verify_that_user_can_login_specific_repository_successfully_with_correct_credentials()
+        {
+            GeneralPage generalPage;
+
+            generalPage = loginPage.LoginDashBoard(Constant.Respos_SampleRepository, Constant.Username_thi, Constant.Password);
+
+            generalPage.VerifyWelComeUserDisplayed(Constant.Username_thi);
+            loginPage = generalPage.LogOut();
+        }
+
+        [TestMethod]
+        public void DA_LOGIN_TC002_Verify_that_user_fails_to_login_with_incorrect_credentials()
+        {
+
+            loginPage.Login(Constant.Respos_SampleRepository, "abc", "abc");
+
+            loginPage.VerifyDashboardErrorMessageLogin(Constant.MsgDashboardErrorLogin);
+        }
+
+        [TestMethod]
+        public void DA_LOGIN_TC003_Verify_that_user_fails_to_login_with_correct_username_and_incorrect_password()
+        {
+
+            loginPage.Login(Constant.Respos_SampleRepository, Constant.UsernameAdmin, "abc");
+
+            loginPage.VerifyDashboardErrorMessageLogin(Constant.MsgDashboardErrorLogin);
+        }
+
+        [TestMethod]
         public void Demo_Thi_DeleteAllPage()
         {
             generalPage = new GeneralPage();
--- SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/LoginTestCases.cs
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/LoginTestCases.cs
@@ -4,38 +4,10 @@ using SeleniumAdvance_Group2.PageObject.General;
 
 namespace SeleniumAdvance_Group2.TestCases
 {
-   // [TestClass]
+    [TestClass]
     public class LoginTestCases : TestBases
     {
-        [TestMethod]
-        public void DA_LOGIN_TC001_Verify_that_user_can_login_specific_repository_successfully_with_correct_credentials()
-        {
-            GeneralPage generalPage;         
-
-            generalPage = loginPage.LoginDashBoard(Constant.Respos_SampleRepository, Constant.Username_thi, Constant.Password);
-                  
-            generalPage.VerifyWelComeUserDisplayed(Constant.Username_thi);
-            loginPage = generalPage.LogOut();
-        }
-
-        [TestMethod]
-        public void DA_LOGIN_TC002_Verify_that_user_fails_to_login_with_incorrect_credentials()
-        {
-          
-            loginPage.Login(Constant.Respos_SampleRepository, "abc", "abc");
-            
-            loginPage.VerifyDashboardErrorMessageLogin(Constant.MsgDashboardErrorLogin);
-        }
-
-        [TestMethod]
-        public void DA_LOGIN_TC003_Verify_that_user_fails_to_login_with_correct_username_and_incorrect_password()
-        {
-          
-            loginPage.Login(Constant.Respos_SampleRepository, Constant.UsernameAdmin, "abc");
-           
-            loginPage.VerifyDashboardErrorMessageLogin(Constant.MsgDashboardErrorLogin);
-        }
-
+        
         [TestMethod]
         public void DA_LOGIN_TC004_Verify_that_user_login_different_repositories_successfully_after_logging_out_current_repository()
         {
