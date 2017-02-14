0b8018b0cd896032c6a1a9abf046c83d6706669f
minhthinguyen <nguyenthiminhthi2009@gmail.com>
2017-02-14 17:42:26 +0700

    hi

---
+++ SeleniumAdvance_Group2_NgocLe/SeleniumAdvance_Group2_NgocLe/TestCases/UnitTest2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumAdvance_Group2.TestCases
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
